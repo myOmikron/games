7544e0f81854b5ae8f1a5b10d11bf6f3b7079e8d
myOmikron <npfister@trufflepig-forensics.de>
2026-09-14 22:54:02 +0200

    Removed bevy_console as it had incompatible versions

--- games/alchemy-game/src/main.rs
+++ games/alchemy-game/src/main.rs
@@ -1,7 +1,6 @@
 use avian2d::PhysicsPlugins;
 use bevy::DefaultPlugins;
 use bevy::app::App;
-use bevy_console::ConsolePlugin;
 use bevy_ecs_ldtk::LdtkPlugin;
 use bevy_enoki::EnokiPlugin;
 use bevy_firefly::app::FireflyPlugin;
@@ -16,7 +15,6 @@ fn main() {
         EnokiPlugin,
         AudioPlugin,
         FireflyPlugin,
-        ConsolePlugin,
         TweeningPlugin,
         YarnSpinnerPlugin::new(),
         PhysicsPlugins::default(),
@@ -27,11 +25,7 @@ fn main() {
         use bevy_inspector_egui::bevy_egui::EguiPlugin;
         use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-        if !app.is_plugin_added::<EguiPlugin>() {
-            app.add_plugins(EguiPlugin::default());
-        }
-
-        app.add_plugins(WorldInspectorPlugin::new());
+        app.add_plugins((EguiPlugin::default(), WorldInspectorPlugin::new()));
     }
 
     app.run();
