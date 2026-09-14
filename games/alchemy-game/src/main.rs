59bdc836a805f6b4ac333edb766c4f47a8146d23
myOmikron <npfister@trufflepig-forensics.de>
2026-09-14 22:27:07 +0200

    Add external plugins

--- games/alchemy-game/src/main.rs
+++ games/alchemy-game/src/main.rs
@@ -1,9 +1,26 @@
+use avian2d::PhysicsPlugins;
 use bevy::DefaultPlugins;
 use bevy::app::App;
+use bevy_console::ConsolePlugin;
+use bevy_ecs_ldtk::LdtkPlugin;
+use bevy_enoki::EnokiPlugin;
+use bevy_firefly::app::FireflyPlugin;
+use bevy_kira_audio::AudioPlugin;
+use bevy_tweening::TweeningPlugin;
+use bevy_yarnspinner::prelude::YarnSpinnerPlugin;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(DefaultPlugins).add_plugins((
+        LdtkPlugin,
+        EnokiPlugin,
+        AudioPlugin,
+        FireflyPlugin,
+        ConsolePlugin,
+        TweeningPlugin,
+        YarnSpinnerPlugin::new(),
+        PhysicsPlugins::default(),
+    ));
 
     #[cfg(feature = "dev")]
     {
